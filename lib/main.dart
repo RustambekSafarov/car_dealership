1c9cb268a59dc19676bbe1f2e3e0296261f7f45d
RustambekSafarov <rustambeksafarovv@gmail.com>
2023-04-07 15:51:01 +0500

    init

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:car_dealership/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Kia.uz',
+      routerConfig: GoRouter(initialLocation: '/', routes: [
+        GoRoute(
+          path: '/',
+          name: HomeScreen.routeName,
+          builder: (context, state) => HomeScreen(),
+        )
+      ]),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  static const routeName = '/';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+    );
+  }
+}
